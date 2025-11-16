4a3173ddf580d7681669dafed697749d44f37551
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2025-11-17 02:30:49 +1000

    justtesting

--- setup.py
+++ setup.py
@@ -56,9 +56,7 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
