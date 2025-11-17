de5e11f1bcfbb254ac8fb5c799edd7842f726528
Morgan Bartholomew <morgan.bartholomew@jetbrains.com>
2025-11-17 14:24:12 +1000

    justtesting

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -4,8 +4,15 @@ fi
 
 mkdir starcraft
 cd starcraft || exit 1
+pwd
+ls -la
+echo "========================================================"
 wget https://blzdistsc2-a.akamaihd.net/Linux/SC2.4.10.zip
 unzip -P iagreetotheeula SC2.4.10.zip
+echo "========================================================"
 rm SC2.4.10.zip
+pwd
+ls -la
+
 cd ..
 cp maps/* starcraft/StarCraftII/Maps/
